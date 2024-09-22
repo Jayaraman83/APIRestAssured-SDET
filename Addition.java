dfa2fd7f5e64c45121e3b18f42f574f60278f1b5
Jayaraman83 <jaymithy8386@gmail.com>
2024-09-23 04:45:50 +1200

    Addition File Commit

---
+++ Addition.java
@@ -0,0 +1,17 @@
+package programs;
+
+public class Addition {
+	
+	public static void main(String[] args) {
+	
+		int a = 10;
+		int b = 20;
+		int c;
+		
+		c = a + b;
+		
+		System.out.println("Addition of two numbers a & b is : " +c);
+		
+	}
+
+}
